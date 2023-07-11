46bdeec226211f7a1ee84e5212c1fcd9d391a78e
Hope Tsunke <ttsunke15@gmail.com>
2023-07-11 15:47:08 +0200

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import troll from "../images/troll-face.png";
 export default function Header() {
 	return (
 		<nav>
-			<img src={troll} className="trollface" />
+			<img src={troll} alt="img" className="trollface" />
 			<h1 className="logo">Meme Genarator</h1>
 		</nav>
 	);
